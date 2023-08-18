948dc346f257dd992da93a955381a4e6eb1058be
hikihun <skh140@naver.com>
2023-08-18 23:23:47 +0900

    feat: 프로그래머스 Lv1 크레인 인형뽑기 게임 풀이코드

---
+++ src/Lv1/크레인_인형뽑기_게임/P64061.java
@@ -0,0 +1,38 @@
+package Lv1.크레인_인형뽑기_게임;
+
+import java.util.Stack;
+
+public class P64061 {
+
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        for (int move : moves) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][move - 1] != 0) {
+                    if (stack.peek() == board[j][move - 1]) {
+                        stack.pop();
+                        answer += 2;
+                    } else {
+                        stack.push(board[j][move - 1]);
+                    }
+                    board[j][move - 1] = 0;
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        P64061 T = new P64061();
+        System.out.println(T.solution(
+            new int[][]{{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2},
+                {3, 5, 1, 3, 1}}, new int[]{1, 5, 3, 5, 1, 2, 1, 4}));
+    }
+
+}
