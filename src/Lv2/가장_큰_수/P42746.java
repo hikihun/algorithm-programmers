84bd8236666c9bb7d6ccd386a01fe1b28196614e
hikihun <skh140@naver.com>
2023-08-28 22:42:34 +0900

    feat: 프로그래머스 Lv2 가장 큰 수 풀이코드

---
+++ src/Lv2/가장_큰_수/P42746.java
@@ -0,0 +1,35 @@
+package Lv2.가장_큰_수;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class P42746 {
+
+    public String solution(int[] numbers) {
+        String answer = "";
+
+        String[] arr = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            arr[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        if (arr[0].equals("0")) {
+            return "0";
+        }
+
+        for (String s : arr) {
+            answer += s;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        P42746 T = new P42746();
+        System.out.println(T.solution(new int[]{6, 10, 2}));
+        System.out.println(T.solution(new int[]{3, 30, 34, 5, 9}));
+
+    }
+
+}
