d562eeea3f5902d7081b8c1b5a4c58899198afb8
hikihun <skh140@naver.com>
2023-12-13 20:21:46 +0900

    백준 Silver 계단 오르기 풀이코드

---
+++ src/Boj/Silver/계단_오르기/Boj2579.java
@@ -0,0 +1,31 @@
+package Boj.Silver.계단_오르기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj2579 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] stair = new int[N + 1];
+        int[] dp = new int[N + 1];
+
+        for (int i = 1; i < N + 1; i++) {
+            stair[i] = Integer.parseInt(br.readLine());
+        }
+        dp[1] = stair[1];
+
+        if (N >= 2) {
+            dp[2] = stair[1] + stair[2];
+        }
+
+        for (int i = 3; i < N + 1; i++) {
+            dp[i] = Math.max(dp[i - 2], dp[i - 3] + stair[i - 1]) + stair[i];
+        }
+
+        System.out.println(dp[N]);
+    }
+
+}
