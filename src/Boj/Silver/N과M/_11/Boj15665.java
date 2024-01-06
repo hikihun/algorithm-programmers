e35c39754a74a5cf2763eceb6ac90c9cda820ae9
hikihun <skh140@naver.com>
2024-01-06 13:08:26 +0900

    백준 Silver N과 M(11) 풀이코드

---
+++ src/Boj/Silver/N과M/_11/Boj15665.java
@@ -0,0 +1,55 @@
+package Boj.Silver.N과M._11;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj15665 {
+
+    static int N, M;
+    static int[] arr, result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        result = new int[M];
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int depth) {
+        if(depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int before = -1;
+        for (int i = 0; i < N; i++) {
+            int now = arr[i];
+            if (before != now) {
+                before = now;
+                result[depth] = arr[i];
+                dfs(depth + 1);
+            }
+
+        }
+    }
+
+}
