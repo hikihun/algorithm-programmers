0c446fc43348d7d7a67dcd3f596643df326ac40a
hikihun <skh140@naver.com>
2024-01-08 23:44:36 +0900

    백준 Silver 등수 구하기 풀이코드

---
+++ src/Boj/Silver/등수_구하기/Boj1205.java
@@ -0,0 +1,39 @@
+package Boj.Silver.등수_구하기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj1205 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int taesoo = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
+
+        int[] score = new int[P];
+        if(N>0) {
+            st = new StringTokenizer(in.readLine());
+            for (int i = 0; i < N; i++) score[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(score);
+
+        if(P==N && score[0]>=taesoo){
+            System.out.println(-1);
+            return;
+        }
+
+        int cnt = 1;
+        for(int i=P-1;i>=Math.max(0,P-N-1);i--){
+            if(score[i]>taesoo) cnt++;
+            else break;
+        }
+        System.out.println(cnt);
+    }
+}
