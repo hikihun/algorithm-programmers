13fd1eeaa3c4f2a1a43a62cf65ac06b32d4d00c5
hikihun <skh140@naver.com>
2024-01-13 22:29:11 +0900

    백준 Silver 서로 다른 부분 문자열의 개수 풀이코드

---
+++ src/Boj/Silver/서로_다른_부분_문자열의_개수/Boj11478.java
@@ -0,0 +1,26 @@
+package Boj.Silver.서로_다른_부분_문자열의_개수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Boj11478 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                set.add(str.substring(i, j));
+            }
+        }
+
+        System.out.println(set.size());
+    }
+
+}
