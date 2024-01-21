deb0d476f727996a93a4cfe177f6b20c7d7f8a62
hikihun <skh140@naver.com>
2024-01-21 19:08:06 +0900

    백준 Silver 종이의 개수 풀이코드

--- src/Boj/Silver/종이의_개수/Boj1780.java
+++ src/Boj/Silver/종이의_개수/Boj1780.java
@@ -3,7 +3,6 @@ package Boj.Silver.종이의_개수;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Boj1780 {
